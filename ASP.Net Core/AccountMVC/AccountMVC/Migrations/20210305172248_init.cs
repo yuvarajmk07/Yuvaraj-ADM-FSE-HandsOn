9d92ee4ce732d7bff0443cd4213d2330180ff73b
Yuvaraj Manikannan <50576506+yuvaraj9999@users.noreply.github.com>
2021-03-05 23:17:01 +0530

    ASP.Net Core

---
+++ ASP.Net Core/AccountMVC/AccountMVC/Migrations/20210305172248_init.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AccountMVC.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.AccountId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Account");
+        }
+    }
+}
